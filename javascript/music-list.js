f69e9ad87de18b1070c82057677a26e43c0bf59d
Denilson <denilson_dias17@hotmail.com>
2025-12-20 21:45:00 -0300

    MarkI

---
+++ javascript/music-list.js
@@ -0,0 +1,115 @@
+let allMusic = [
+
+    {
+        name: "POWER GOT A NAME",
+        artist: "Infinity Love",
+        img: "Purple",
+        src: "Purple"
+    },
+
+    {
+        name: "TIME LOOP",
+        artist: "Infinity Love",
+        img: "Green",
+        src: "Green"
+    },
+
+    {
+        name: "STILL FALLING",
+        artist: "Infinity Love",
+        img: "Yellow",
+        src: "Yellow"
+    },
+
+    {
+        name: "SOUL TO SOUL",
+        artist: "Infinity Love",
+        img: "Orange",
+        src: "Orange"
+    },
+
+    {
+        name: "MY PLACE IS YOU",
+        artist: "Infinity Love",
+        img: "Blue",
+        src: "Blue"
+    },
+
+    {
+        name: "REAL QUEEN",
+        artist: "Infinity Love",
+        img: "Red",
+        src: "Red"
+    },
+
+    {
+        name: "INFINITE LOVE",
+        artist: "Infinity Love",
+        img: "Gauntlet",
+        src: "Gauntlet"
+    },
+
+    {
+        name: "VOID",
+        artist: "Infinity Love",
+        img: "Black",
+        src: "Black"
+    },
+
+    {
+        name: "HEART WITHOUT ARMOR",
+        artist: "My feelings were set free for you",
+        img: "Armor",
+        src: "Armor"
+    },
+
+    {
+        name: "ALWAYS YOU",
+        artist: "Just you and me",
+        img: "Shield",
+        src: "Shield"
+    },
+
+    {
+        name: "HARD CALM",
+        artist: "My lovely little storm",
+        img: "Angry",
+        src: "Angry"
+    },
+
+    {
+        name: "ONE WORLD LEFT",
+        artist: "You and me forever",
+        img: "Avengers",
+        src: "Avengers"
+    },
+
+    {
+        name: "HOLD ME HERE",
+        artist: "I will always hold you",
+        img: "Thunder",
+        src: "Thunder"
+    },
+    
+    {
+        name: "NO BRAKES (EXPLICIT)",
+        artist: "I love our intensity",
+        img: "Explicit",
+        src: "Explicit"
+    },
+
+    {
+        name: "ELEVEN WAYS TO LOVE YOU",
+        artist: "Our number, our story",
+        img: "Magic",
+        src: "Magic"
+    },
+
+    {
+        name: "GREAT RESPONSIBILITIES",
+        artist: "I always knew it was you",
+        img: "Responsibilities",
+        src: "Responsibilities"
+    },
+
+]
